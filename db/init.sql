486ac0094da1898e1be9bed83d1f43deaf9f819c
wei <wei@o-r-g.com>
2022-06-30 21:58:59 +0800

    clean up 2

---
+++ db/init.sql
@@ -0,0 +1,133 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos12.2 (arm64)
+--
+-- Host: localhost    Database: subscriptions_servinglibrary_local
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `active` int unsigned NOT NULL DEFAULT '1',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `object` int unsigned DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `rank` int unsigned DEFAULT NULL,
+  `type` varchar(10) NOT NULL DEFAULT 'jpg',
+  `caption` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `objects`
+--
+
+DROP TABLE IF EXISTS `objects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `objects` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `active` int unsigned NOT NULL DEFAULT '1',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `rank` int unsigned DEFAULT NULL,
+  `name1` tinytext,
+  `name2` tinytext,
+  `address1` text,
+  `address2` text,
+  `city` tinytext,
+  `state` tinytext,
+  `zip` tinytext,
+  `country` tinytext,
+  `phone` tinytext,
+  `fax` tinytext,
+  `url` tinytext,
+  `email` tinytext,
+  `begin` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `head` tinytext,
+  `deck` mediumblob,
+  `body` mediumblob,
+  `notes` mediumblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `objects`
+--
+
+LOCK TABLES `objects` WRITE;
+/*!40000 ALTER TABLE `objects` DISABLE KEYS */;
+INSERT INTO `objects` VALUES (1,1,'2022-06-30 09:54:22','2022-06-30 09:54:22',NULL,'_Reports',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'reports',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,1,'2022-06-30 09:54:39','2022-06-30 09:54:39',NULL,'Subscribers',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'subscribers',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `objects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `wires`
+--
+
+DROP TABLE IF EXISTS `wires`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `wires` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `active` int unsigned NOT NULL DEFAULT '1',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `fromid` int unsigned DEFAULT NULL,
+  `toid` int unsigned DEFAULT NULL,
+  `weight` float NOT NULL DEFAULT '1',
+  `notes` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wires`
+--
+
+LOCK TABLES `wires` WRITE;
+/*!40000 ALTER TABLE `wires` DISABLE KEYS */;
+INSERT INTO `wires` VALUES (1,1,'2022-06-30 09:54:22','2022-06-30 09:54:22',0,1,1,NULL),(2,1,'2022-06-30 09:54:39','2022-06-30 09:54:39',0,2,1,NULL);
+/*!40000 ALTER TABLE `wires` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-30 21:55:05
